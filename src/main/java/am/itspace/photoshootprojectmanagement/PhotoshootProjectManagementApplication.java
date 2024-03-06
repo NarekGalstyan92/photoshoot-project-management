31d8f91e160d5e20bc29419478bdcf48ad9fb3e3
narekgalstyan <narekgalstyan92@gmail.com>
2024-03-06 11:27:51 -0800

    Photoshoot Project Management V 1.0.3
    Added:
    - application.yml
    - change-log.xml
    
    Modified:
    - Main class: added Scheduling and Synchronize annotations
    
    Removed:

--- src/main/java/am/itspace/photoshootprojectmanagement/PhotoshootProjectManagementApplication.java
+++ src/main/java/am/itspace/photoshootprojectmanagement/PhotoshootProjectManagementApplication.java
@@ -2,12 +2,15 @@ package am.itspace.photoshootprojectmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class PhotoshootProjectManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoshootProjectManagementApplication.class, args);
     }
-
 }
