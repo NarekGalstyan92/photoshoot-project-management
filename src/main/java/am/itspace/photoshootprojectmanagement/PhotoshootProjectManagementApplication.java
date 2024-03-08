6a55a179d6bb8ea0261cc8d24aed3fc378895d9c
narekgalstyan <narekgalstyan92@gmail.com>
2024-03-08 03:18:02 -0800

    Photoshoot Project Management V 1.0.3
    Added:
    - new changeSets
    - created event_category, review, image tables
    - added "role" and "is_deleted" columns in "user" table
    
    Modified:
    - "category" enum from booking table removed and was set as individual table
    
    Removed:
    - Main class: removed Scheduling and Synchronize annotations

--- src/main/java/am/itspace/photoshootprojectmanagement/PhotoshootProjectManagementApplication.java
+++ src/main/java/am/itspace/photoshootprojectmanagement/PhotoshootProjectManagementApplication.java
@@ -2,12 +2,9 @@ package am.itspace.photoshootprojectmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
 public class PhotoshootProjectManagementApplication {
 
     public static void main(String[] args) {
